3f8dc9b52addce614837bb2daa9a83520deb9b61
Teddaudi <daudited@gmail.com>
2024-10-16 19:41:18 +0300

    edit the middleware

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,9 @@ export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
     const isPublicPath = path === '/signin' || path === '/signup';
     const token = request.cookies.get('token')?.value || '';
-    if (isPublicPath && token) {
-        return NextResponse.redirect(new URL('/', request.nextUrl))
-    }
+    // if (isPublicPath && token) {
+    //     return NextResponse.redirect(new URL('/', request.nextUrl))
+    // }
     if (!isPublicPath && !token) {
         return NextResponse.redirect(new URL('/signin', request.nextUrl))
     }
