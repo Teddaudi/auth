fb91d3b40ca8d3bab4b42755dcd59b1929fc2edc
Teddaudi <daudited@gmail.com>
2024-10-30 06:49:41 +0300

    created not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
 import Link from 'next/link'
-
-const Page = () => {
+ 
+export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center">
-      <p className="mb-4">Create an account or sign in if you already have one</p>
+      <p className="mb-4">Not Found</p>
       <Link href="/profile" className="text-green-500">Return To Profile</Link>
     </div>
   )
 }
-
-export default Page
